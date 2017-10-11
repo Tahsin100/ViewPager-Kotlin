eb9fd8e8da40cfad0474539fd618b050c653ee0f
riddhiman.adib <riddhiman.adib@harriken.com>
2017-10-11 18:15:50 +0600

    first commit

---
+++ app/src/main/java/me/tahsinrupam/viewpagerkotlin/MainActivity.kt
@@ -0,0 +1,14 @@
+package me.tahsinrupam.viewpagerkotlin
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.support.v4.view.ViewPager
+
+class MainActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ app/src/main/java/me/tahsinrupam/viewpagerkotlin/adapters/CustomPagerAdapter.kt
@@ -0,0 +1,42 @@
+package me.tahsinrupam.viewpagerkotlin.adapters
+
+import android.support.v4.app.Fragment
+import android.support.v4.app.FragmentManager
+import android.support.v4.app.FragmentStatePagerAdapter
+import me.tahsinrupam.viewpagerkotlin.fragments.ViewpagerFragment1
+import me.tahsinrupam.viewpagerkotlin.fragments.ViewpagerFragment2
+import me.tahsinrupam.viewpagerkotlin.fragments.ViewpagerFragment3
+
+/**
+ * Created by Tahsin on 11-Oct-17.
+ */
+
+
+class CustomPagerAdapter(fm: FragmentManager?) : FragmentStatePagerAdapter(fm) {
+
+
+    private val NUM_PAGES = 3
+    override fun getItem(position: Int): Fragment? {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+
+        when(position) {
+            0 ->
+                return ViewpagerFragment1()
+            1 ->
+                return ViewpagerFragment2()
+            2 ->
+                return ViewpagerFragment3()
+            else ->
+                return null
+        }
+
+
+    }
+
+    override fun getCount(): Int {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return NUM_PAGES
+    }
+
+
+}
