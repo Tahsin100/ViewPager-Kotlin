d6ca812fc4ae78ca0b9d105065bccb9fb39e27f4
riddhiman.adib <riddhiman.adib@harriken.com>
2017-10-15 15:58:28 +0600

    Viewpager complete

--- app/src/main/java/me/tahsinrupam/viewpagerkotlin/MainActivity.kt
+++ app/src/main/java/me/tahsinrupam/viewpagerkotlin/MainActivity.kt
@@ -7,6 +7,7 @@ import android.support.v4.view.ViewPager
 import android.support.v7.app.ActionBar.NAVIGATION_MODE_TABS
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.activity_main.view.*
+import me.tahsinrupam.viewpagerkotlin.R.styleable.Toolbar
 import me.tahsinrupam.viewpagerkotlin.adapters.CustomPagerAdapter
 
 class MainActivity : AppCompatActivity() {
@@ -15,6 +16,10 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        toolbar.title = "ViewPager"
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+
         val adapter = CustomPagerAdapter(supportFragmentManager)
         viewpager.adapter = adapter
         tabLayout.setupWithViewPager(viewpager)
